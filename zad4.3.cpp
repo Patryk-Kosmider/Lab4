f565f9efb6bf373b8760d9c553b4422c13b1bb7a
Patryk-Kosmider <115877789+Patryk-Kosmider@users.noreply.github.com>
2022-12-18 16:55:03 +0100

    Add files via upload

---
+++ zad4.3.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+int main()
+{
+
+	int tab[10][10];
+
+
+	for(int i = 0; i < 10; i++)
+	{
+
+		tab[i][0] = i;
+
+	}
+
+	for(int i = 0; i < 10; i++)
+	{
+
+		tab[i][1] = i + i;
+
+	}
+
+	for(int i = 0; i < 10; i++)
+	{
+
+		tab[i][2] = i * i;
+
+
+	}
+
+	for(int i = 0; i < 10; i++)
+	{
+
+		tab[i][3] = 3 + i;
+
+	}
+
+	for(int i = 0; i < 10; i++)
+	{
+		tab[i][4] = i - 4;
+
+	}
+
+	for(int i = 5; i < 10; i++)
+	{
+		for(int j = 0; j < 10; j ++)
+		{
+			tab[j][i] = 0;
+		}
+		
+
+	}
+
+	for(int i = 0; i < 10; i++)
+	{
+		for(int j = 0; j < 10; j++)
+		{
+			cout << setw(5) << tab[i][j] << setw(5);
+		}
+		cout << endl;
+	}
+	
+
+
+	return 0;
+}
