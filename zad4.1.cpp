cfb5c0929b62703baa98063995f9db833665bf2e
Patryk-Kosmider <115877789+Patryk-Kosmider@users.noreply.github.com>
2022-12-18 17:05:14 +0100

    Add files via upload

---
+++ zad4.1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int tab[100];
+	int n;
+	int el;
+	cout << "Podaj ilosc elementow: " << endl;
+	cin >> n;
+
+	for(int i = 0; i < n; i++)
+	{
+		cout << "Podaj element: " << endl;
+		cin >> el;
+		tab[i] = el;
+
+	}
+
+	int max = tab[0];
+	for (int i = 0; i < n; i++)
+	{
+		if(max < tab[i])
+		{
+			max = tab[i];
+		}
+
+	}
+
+	cout << "Najwiekszy element tablicy to: " << max << endl;
+
+
+
+	return 0;
+}
