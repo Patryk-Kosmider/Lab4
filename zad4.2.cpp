bffe13655819dc7e0cf2a4f65bdd850afa9cd4d1
Patryk-Kosmider <115877789+Patryk-Kosmider@users.noreply.github.com>
2022-12-18 17:20:21 +0100

    Add files via upload

---
+++ zad4.2.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+    int n;
+    cout << "Podaj ilosc elementow tablicy: " << endl;
+    cin >> n;
+    int tab[100];
+
+    int x;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Podaj liczbe: " << endl;
+        cin >> x;
+        tab[i] = x;
+
+    }
+
+    int nm;
+    int ilosc = 0;
+
+    int pomoc[100];
+
+    for (int i = 0; i < n; i++)
+    {
+        nm = tab[i];
+
+        for (int j = 0; j < n; j++)
+        {
+
+            if (nm == tab[j])
+            {
+                ilosc += 1;
+                pomoc[i] = ilosc;
+            }
+
+
+
+        }
+        ilosc = 0;
+
+    }
+
+
+    int start = pomoc[0];
+    int miejsce = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (pomoc[0] < pomoc[i])
+        {
+            start = pomoc[i];
+            miejsce = i;
+        }
+
+    }
+
+    cout << "Najwiecej powtorzen ma liczba: " << tab[miejsce] << endl;
+    cout << "Ilosc powtorzen to: " << pomoc[miejsce] << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
